b88bd0d4f5cf9c3181068ae579b4a7ee929271b3
Boris Nikolic <boris@primer.io>
2023-08-03 14:17:03 +0200

    Update freamework version and example project

--- Example/Pods/Target Support Files/Primer3DS/Primer3DS-xcframeworks.sh
+++ Example/Pods/Target Support Files/Primer3DS/Primer3DS-xcframeworks.sh
@@ -17,10 +17,10 @@ RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
 variant_for_slice()
 {
   case "$1" in
-  "ThreeDS_SDK.xcframework/ios-arm64_armv7")
+  "ThreeDS_SDK.xcframework/ios-arm64")
     echo ""
     ;;
-  "ThreeDS_SDK.xcframework/ios-arm64_i386_x86_64-simulator")
+  "ThreeDS_SDK.xcframework/ios-arm64_x86_64-simulator")
     echo "simulator"
     ;;
   esac
@@ -29,11 +29,11 @@ variant_for_slice()
 archs_for_slice()
 {
   case "$1" in
-  "ThreeDS_SDK.xcframework/ios-arm64_armv7")
-    echo "arm64 armv7"
+  "ThreeDS_SDK.xcframework/ios-arm64")
+    echo "arm64"
     ;;
-  "ThreeDS_SDK.xcframework/ios-arm64_i386_x86_64-simulator")
-    echo "arm64 i386 x86_64"
+  "ThreeDS_SDK.xcframework/ios-arm64_x86_64-simulator")
+    echo "arm64 x86_64"
     ;;
   esac
 }
@@ -117,5 +117,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/../../Sources/Frameworks/ThreeDS_SDK.xcframework" "Primer3DS" "framework" "ios-arm64_armv7" "ios-arm64_i386_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/../../Sources/Frameworks/ThreeDS_SDK.xcframework" "Primer3DS" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
 
