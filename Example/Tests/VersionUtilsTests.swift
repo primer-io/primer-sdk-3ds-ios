10c7cb369377cee9303f70b176d093f6b2f2b2a1
Niall Quinn <niall@primer.io>
2023-10-18 13:36:43 +0200

    Add unit test for versions

---
+++ Example/Tests/VersionUtilsTests.swift
@@ -0,0 +1,32 @@
+//
+//  VersionUtilsTests.swift
+//  Primer3DS_Tests
+//
+//  Created by Niall Quinn on 18/10/23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import Primer3DS
+import ThreeDS_SDK
+
+final class VersionUtilsTests: XCTestCase {
+
+    func test_wrapperVersionNumber() throws {
+        XCTAssertEqual(VersionUtils.wrapperSDKVersionNumber, Primer3DSVersion)
+    }
+    
+    func test_3DSVersionNumber() throws {
+        XCTAssertEqual(VersionUtils.threeDsSdkVersionNumber, NetceteraSDKVersion)
+    }
+
+    func test_3DSMatchesNetcetera() throws {
+        guard let netceteraVersion = Bundle(for: ThreeDS2ServiceSDK.self)
+            .infoDictionary?["CFBundleShortVersionString"] as? String else {
+            XCTFail("Could not locate ThreeDS Bundle")
+            return
+        }
+        
+        XCTAssertEqual(netceteraVersion, VersionUtils.threeDsSdkVersionNumber)
+    }
+}
