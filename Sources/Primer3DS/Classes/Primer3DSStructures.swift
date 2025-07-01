55180dac8d4d7ec28e1a53e7be7a6c280aa8cd2d
Niall Quinn <niall@primer.io>
2025-07-01 14:29:00 +0200

    fix: Add CARTES_BANCAIRES DSID Mapping (#60)
    * Add CARTES_BANCAIRES and route MAESTRO to MASTERCARD
    
    * Review feedback
    
    * Correct capitalization
    
    * Clean up switch

--- Sources/Primer3DS/Classes/Primer3DSStructures.swift
+++ Sources/Primer3DS/Classes/Primer3DSStructures.swift
@@ -13,19 +13,21 @@ public enum Environment: String, Codable {
 
 public enum DirectoryServerNetwork: String {
     case masterCard = "MASTERCARD"
+    case maestro = "MAESTRO"
     case visa = "VISA"
     case amex = "AMEX"
     case jcb = "JCB"
     case diners = "DINERS_CLUB"
     case discover = "DISCOVER"
     case unionpay = "UNIONPAY"
+    case cartesBancaires = "CARTES_BANCAIRES"
     case unknown = "UNKNOWN"
 
     var directoryServerId: String? {
         switch self {
         case .visa:
             return DsRidValues.visa
-        case .masterCard:
+        case .masterCard, .maestro:
             return DsRidValues.mastercard
         case .amex:
             return DsRidValues.amex
@@ -35,6 +37,8 @@ public enum DirectoryServerNetwork: String {
             return DsRidValues.diners
         case .unionpay:
             return DsRidValues.union
+        case .cartesBancaires:
+            return DsRidValues.cartesBancaires
         case .unknown:
             return nil
         }
@@ -45,21 +49,19 @@ public enum DirectoryServerNetwork: String {
     }
 }
 
-@objc internal class SDKAuthData: NSObject, Primer3DSSDKGeneratedAuthData {
-    
+@objc final class SDKAuthData: NSObject, Primer3DSSDKGeneratedAuthData {
     var sdkAppId: String
     var sdkTransactionId: String
     var sdkTimeout: Int
     var sdkEncData: String
     var sdkEphemPubKey: String
     var sdkReferenceNumber: String
-    
+
     init(sdkAppId: String, sdkTransactionId: String, sdkTimeout: Int, sdkEncData: String, sdkEphemPubKey: String, sdkReferenceNumber: String) {
         self.sdkAppId = sdkAppId
         self.sdkTransactionId = sdkTransactionId
         self.sdkTimeout = sdkTimeout
         self.sdkEncData = sdkEncData
-        self.sdkEphemPubKey = sdkEphemPubKey
         self.sdkEphemPubKey = sdkEphemPubKey
         self.sdkReferenceNumber = sdkReferenceNumber
         super.init()
@@ -67,12 +69,11 @@ public enum DirectoryServerNetwork: String {
 }
 
 @objc public class SDKAuthResult: NSObject {
-    
     static let sdkMaxTimeout: Int = 10
-    
+
     public var authData: Primer3DSSDKGeneratedAuthData
     public var maxSupportedThreeDsProtocolVersion: String
-    
+
     init(authData: Primer3DSSDKGeneratedAuthData, maxSupportedThreeDsProtocolVersion: String) {
         self.authData = authData
         self.maxSupportedThreeDsProtocolVersion = maxSupportedThreeDsProtocolVersion
@@ -80,37 +81,31 @@ public enum DirectoryServerNetwork: String {
     }
 }
 
-@objc internal class AuthCompletion: NSObject, Primer3DSCompletion {
+@objc class AuthCompletion: NSObject, Primer3DSCompletion {
     public let sdkTransactionId: String
     public let transactionStatus: String
-    
+
     init(sdkTransactionId: String, transactionStatus: String) {
         self.sdkTransactionId = sdkTransactionId
         self.transactionStatus = transactionStatus
     }
 }
 
-internal enum AuthenticationStatus: String {
+enum AuthenticationStatus: String {
     case y, a, n, u, e
-    
+
     init(rawValue: String) {
         switch rawValue.lowercased() {
-        case "y":
-            self = AuthenticationStatus.y
-        case "a":
-            self = AuthenticationStatus.a
-        case "n":
-            self = AuthenticationStatus.n
-        case "u":
-            self = AuthenticationStatus.u
-        case "e":
-            self = AuthenticationStatus.e
-        default:
-            self = AuthenticationStatus.e
+        case "y": self = .y
+        case "a": self = .a
+        case "n": self = .n
+        case "u": self = .u
+        case "e": self = .e
+        default: self = .e
         }
     }
-    
-    var `description`: String {
+
+    var description: String {
         switch self {
         case .y:
             return "Authentication successful"
@@ -124,7 +119,7 @@ internal enum AuthenticationStatus: String {
             return "Error"
         }
     }
-    
+
     var recommendation: AuthenticationRecommendation {
         switch self {
         case .y,
@@ -139,7 +134,6 @@ internal enum AuthenticationStatus: String {
     }
 }
 
-internal enum AuthenticationRecommendation {
+enum AuthenticationRecommendation {
     case proceed, stop, merchantDecision
 }
-
